980d80e82fe6942088db21c5699b242cd790a1fe
Jeffrey Kwok <jeffreyckwok@gmail.com>
2017-04-24 12:53:09 -0700

    changed filepaths

--- courses/settings.py
+++ courses/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'main.urls'
+ROOT_URLCONF = 'courses.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'main.wsgi.application'
+WSGI_APPLICATION = 'courses.wsgi.application'
 
 
 # Database
